d9216904df850a819ca57c2d800b73c30dbd4afe
Marco Fischersworring <mfiwo@web.de>
2015-11-19 23:37:21 +0000

    step 2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,7 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create(:title => movie[:title], :release_date => movie[:release_date], :rating => movie[:rating])
+    unless Movie.exists?(:title => movie[:title])
+      Movie.create(:title => movie[:title], :release_date => movie[:release_date], :rating => movie[:rating])
+    end
   end
   #fail "Unimplemented"
 end
@@ -26,10 +28,20 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_arr = rating_list.split(%r{,\s*})
+  rating_arr.each do |rating|
+    if uncheck == 'un'
+      uncheck("ratings[#{rating}]")
+    else
+      check("ratings[#{rating}]")
+    end
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  movie_count = Movie.count
+  page.assert_selector('table#movies tbody tr', :count => movie_count)
+  #fail "Unimplemented"
 end
